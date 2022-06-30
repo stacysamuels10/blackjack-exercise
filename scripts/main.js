37e9bebe4423b03981bc43a0ea2daec8c2827e64
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-30 11:31:20 -0500

    project

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,47 @@
-window.addEventListener('DOMContentLoaded', function() {
-  // Execute after page load
-})
+const dealerHand = document.getElementById("dealer-hand");
+const playerHand = document.getElementById("player-hand");
+const deck = [];
+const suits = ["hearts", "spades", "clubs", "diamonds"];
+const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+const makeDeck = (rank, suit) => {
+  let pointValue = rank;
+
+  const card = {
+    rank: rank,
+    suit: suit,
+    pointValue: rank > 10 ? 10 : rank,
+  };
+  deck.push(card);
+};
+for (let suit of suits) {
+  for (const rank of ranks) {
+    makeDeck(rank, suit);
+  }
+}
+
+const getCard = () => {
+  const dealtCard = Math.floor(Math.random() * deck.length - 1) + 1;
+  return deck.splice(dealtCard, 1)[0];
+};
+
+const dealerStartHand = () => {
+  const dealerCard1 = getCard();
+  const dCard1num = Object.values(dealerCard1)[0];
+  const dCard1suit = Object.values(dealerCard1)[1];
+  const dealerCard2 = getCard();
+  const dCard2num = Object.values(dealerCard2)[0];
+  const dCard2suit = Object.values(dealerCard2)[1];
+  sum = dCard1num + dCard2num;
+  console.log("you're hand adds up to", sum);
+};
+
+const playerStartHand = () => {
+  const playerCard1 = getCard();
+  const playerCard2 = getCard();
+};
+
+dealerStartHand();
+playerStartHand();
+
+window.addEventListener("DOMContentLoaded", function () {});
