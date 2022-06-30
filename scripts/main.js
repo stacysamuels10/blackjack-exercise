b72a4ed7991961c902f79ff3b9472e947c4c36e6
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-30 13:31:52 -0500

    blackjack morning

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
-const deck = [];
+let deck = [];
 const suits = ["hearts", "spades", "clubs", "diamonds"];
 const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+const startGame = document.getElementById("deal-button");
+
+let dealerCards = [];
+let playerCards = [];
 
 const makeDeck = (rank, suit) => {
   let pointValue = rank;
@@ -14,34 +18,63 @@ const makeDeck = (rank, suit) => {
   };
   deck.push(card);
 };
-for (let suit of suits) {
-  for (const rank of ranks) {
-    makeDeck(rank, suit);
+const makeNewDeck = () => {
+  for (let suit of suits) {
+    for (const rank of ranks) {
+      makeDeck(rank, suit);
+    }
   }
-}
-
+};
 const getCard = () => {
   const dealtCard = Math.floor(Math.random() * deck.length - 1) + 1;
   return deck.splice(dealtCard, 1)[0];
 };
 
-const dealerStartHand = () => {
-  const dealerCard1 = getCard();
-  const dCard1num = Object.values(dealerCard1)[0];
-  const dCard1suit = Object.values(dealerCard1)[1];
-  const dealerCard2 = getCard();
-  const dCard2num = Object.values(dealerCard2)[0];
-  const dCard2suit = Object.values(dealerCard2)[1];
-  sum = dCard1num + dCard2num;
-  console.log("you're hand adds up to", sum);
+let dealrunningSum = (dealerCards) => {
+  sum = 0;
+  for (card of dealerCards) {
+    const num = card.pointValue;
+    sum += num;
+  }
+  console.log(sum);
 };
 
-const playerStartHand = () => {
-  const playerCard1 = getCard();
-  const playerCard2 = getCard();
+const addPlayerCard = () => {
+  const card = getCard();
+  playerCards.push(card);
+  renderCard(card, playerHand);
 };
 
-dealerStartHand();
-playerStartHand();
+const addDealerCard = () => {
+  const card = getCard();
+  dealerCards.push(card);
+  renderCard(card, dealerHand);
+};
+
+const gameStart = () => {
+  dealerHand.innerHTML = null;
+  playerHand.innerHTML = null;
+  dealerCards = [];
+  playerCards = [];
+  deck = [];
+  makeNewDeck();
+  addPlayerCard();
+  addDealerCard();
+  addPlayerCard();
+  addDealerCard();
+  console.log("dealer", dealerCards);
+  console.log("player", playerCards);
+  console.log(deck);
+  dealrunningSum(dealerCards);
+};
+
+const renderCard = (card, targetElement) => {
+  const img = document.createElement("img");
+  img.dataset.suit = card.suit;
+  img.dataset.rank = card.rank;
+  targetElement.append(img);
+};
+
+startGame.onclick = gameStart;
 
 window.addEventListener("DOMContentLoaded", function () {});
